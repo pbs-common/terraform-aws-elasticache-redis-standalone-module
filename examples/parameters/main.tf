6948e6dfd56c0b5cc3b7d959e053a1c66e32a764
ccioltan <cristian.cioltan@3pillarglobal.com>
2025-12-16 10:41:58 +0200

    CAT-25878 - Update for terraform-aws-elasticache-parameter-group-module, adding additional required parameters

--- examples/parameters/main.tf
+++ examples/parameters/main.tf
@@ -13,7 +13,13 @@ module "redis" {
 }
 
 module "parameters" {
-  source = "github.com/pbs/terraform-aws-elasticache-parameter-group-module?ref=1.0.0"
+  source = "github.com/pbs-common/terraform-aws-elasticache-parameter-group-module?ref=1.0.0"
 
   name = var.product
+
+  organization = var.organization
+  environment  = var.environment
+  product      = var.product
+  repo         = var.repo
+  owner        = var.owner
 }
